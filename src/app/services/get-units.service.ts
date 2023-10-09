b9a51a4c0cfd57ee55973b4ddf9702e227c4f1f0
devruchoa <ronaldo.uchoa.pessoal@gmail.com>
2023-10-08 21:39:01 -0300

    feat: filter

--- src/app/services/get-units.service.ts
+++ src/app/services/get-units.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { UnitsResponse } from '../types/units-response.interface';
+import { Location } from '../types/location.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,26 @@ import { UnitsResponse } from '../types/units-response.interface';
 export class GetUnitsService {
   readonly API_URL = 'https://test-frontend-developer.s3.amazonaws.com/data/locations.json';
 
-  constructor(private httpClient: HttpClient) { }
+  private allUnitsSubject: BehaviorSubject<Location[]>= new BehaviorSubject<Location[]>([]);
+  private allUnits$: Observable<Location[]> = this.allUnitsSubject.asObservable();
+  private filteredUnits: Location[] = [];
 
-  getAllUnits(): Observable<UnitsResponse> {
-    return this.httpClient.get<UnitsResponse>(this.API_URL);
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get<UnitsResponse>(this.API_URL).subscribe((data) => {
+      this.allUnitsSubject.next(data.locations);
+      this.filteredUnits = data.locations;
+    });
+  }
+
+  getAllUnits(): Observable<Location[]> {
+    return this.allUnits$;
+  }
+
+  getFilteredUnits(): Location[] {
+    return this.filteredUnits;
+  }
+
+  setFilteredUnits(units: Location[]): void {
+    this.filteredUnits = units;
   }
 }
